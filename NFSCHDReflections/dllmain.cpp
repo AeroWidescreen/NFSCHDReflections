5d0315c80bbd4c73096b9c187c61b4f5641f4ce9
Aero_ <19331642+AeroWidescreen@users.noreply.github.com>
2019-02-26 19:34:04 -0800

    Initial commit

---
+++ NFSCHDReflections/dllmain.cpp
@@ -0,0 +1,61 @@
+﻿#include "stdafx.h"
+#include "stdio.h"
+#include <windows.h>
+#include "..\includes\injector\injector.hpp"
+#include <cstdint>
+#include "..\includes\IniReader.h"
+#include <d3d9.h>
+
+DWORD WINAPI Thing(LPVOID);
+
+bool HDReflections, PseudoXbox360Reflections;
+static int ResolutionX, ResolutionY;
+DWORD GameState;
+HWND windowHandle;
+
+void Init()
+{
+	// Read values from .ini
+	CIniReader iniReader("NFSCHDReflections.ini");
+
+	// Resolution
+	ResolutionX = iniReader.ReadInteger("RESOLUTION", "ResolutionX", 1920);
+	ResolutionY = iniReader.ReadInteger("RESOLUTION", "ResolutionY", 1080);
+
+	// General
+	HDReflections = iniReader.ReadInteger("GENERAL", "HDReflections", 1);
+	PseudoXbox360Reflections = iniReader.ReadInteger("GENERAL", "PseudoXbox360Reflections", 1);
+
+	if (HDReflections)
+	{
+		// Vehicle Reflection
+		injector::WriteMemory<uint32_t>(0x70DE39, ResolutionY, true);
+	}
+
+	if (PseudoXbox360Reflections)
+	{
+		injector::WriteMemory<uint8_t>(0x72ECE4, 0x02, true);
+	}
+}
+	
+
+BOOL APIENTRY DllMain(HMODULE /*hModule*/, DWORD reason, LPVOID /*lpReserved*/)
+{
+	if (reason == DLL_PROCESS_ATTACH)
+	{
+		uintptr_t base = (uintptr_t)GetModuleHandleA(NULL);
+		IMAGE_DOS_HEADER* dos = (IMAGE_DOS_HEADER*)(base);
+		IMAGE_NT_HEADERS* nt = (IMAGE_NT_HEADERS*)(base + dos->e_lfanew);
+
+		if ((base + nt->OptionalHeader.AddressOfEntryPoint + (0x400000 - base)) == 0x87E926) // Check if .exe file is compatible - Thanks to thelink2012 and MWisBest
+			Init();
+
+		else
+		{
+			MessageBoxA(NULL, "This .exe is not supported.\nPlease use v1.4 English nfsc.exe (6,88 MB (7.217.152 bytes)).", "NFSC HD Reflections", MB_ICONERROR);
+			return FALSE;
+		}
+	}
+	return TRUE;
+	
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# NFSC HD Reflections
+
+## Features
+- ResolutionX : Controls the horizontal resolution of reflections.  
+- ResolutionY : Controls the vertical resolution of reflections. 
+- HDReflections : Reflection resolution will be increased based on the values set.  
+- PseudoXbox360Reflections : Fakes the look of Xbox 360 vehicle reflections by making the road textures invisible.
+
+## Credits
+[Aero_](https://github.com/AeroWidescreen) : Creator of NFSC HD Reflections  
+[ExOpts Team](https://github.com/ExOptsTeam/) : The majority of the project is based on their work. This wouldn't exist without them.  
