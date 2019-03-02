e37439181f766ff21a962c8c607687dbcc29ba12
Aero_ <19331642+AeroWidescreen@users.noreply.github.com>
2019-03-01 23:45:33 -0800

    reflection blur strength

--- NFSCHDReflections/dllmain.cpp
+++ NFSCHDReflections/dllmain.cpp
@@ -8,13 +8,20 @@
 
 DWORD WINAPI Thing(LPVOID);
 
-bool HDReflections, PseudoXbox360Reflections, DisableReflectionBlur;
+bool HDReflections, PseudoXbox360Reflections;
 static int ResolutionX, ResolutionY;
+static float ReflectionBlurStrength;
+static float DownScale4x4Strength = 2.0f;
 DWORD GameState;
 HWND windowHandle;
 
 DWORD RoadReflectionCodeCaveExit1 = 0x71AA2B;
 DWORD RoadReflectionCodeCaveExit2 = 0x71AA7B;
+DWORD ReflectionBlurStrengthCodeCaveExit = 0x73C982;
+DWORD DownScale4x4ReturnAddress;
+DWORD DownScale4x4ReturnAddressCodeCaveExit = 0x748886;
+DWORD DownScale4x4StrengthCodeCaveExit1 = 0x73CC2F;
+DWORD DownScale4x4StrengthCodeCaveExit2 = 0x73CC43;
 
 void __declspec(naked) RoadReflectionCodeCave1()
 {
@@ -38,6 +45,64 @@ void __declspec(naked) RoadReflectionCodeCave2()
 	}
 }
 
+void __declspec(naked) ReflectionBlurStrengthCodeCave()
+{
+	__asm {
+		fdivr dword ptr ds : [ReflectionBlurStrength]
+		jmp ReflectionBlurStrengthCodeCaveExit
+	}
+}
+
+void __declspec(naked) DownScale4x4ReturnAddressCodeCave()
+{
+	__asm {
+		// gets return address
+		push eax
+		mov eax, dword ptr ds : [esp + 0x4]
+		mov dword ptr ds : [DownScale4x4ReturnAddress], eax
+		pop eax
+		push ebp
+		mov ebp, esp
+		and esp, 0xFFFFFFF0
+		jmp DownScale4x4ReturnAddressCodeCaveExit
+	}
+}
+
+
+void __declspec(naked) DownScale4x4StrengthCodeCave1()
+{
+	__asm {
+		// horizontal road reflection
+		cmp dword ptr ds : [DownScale4x4ReturnAddress], 0x72E22E
+		jne DownScale4x4StrengthCodeCave1Part2
+		fdivr dword ptr ds : [ReflectionBlurStrength]
+		jmp DownScale4x4StrengthCodeCaveExit1
+		
+	DownScale4x4StrengthCodeCave1Part2 :
+		// restores horizontal depth of field
+		fdivr dword ptr ds : [DownScale4x4Strength]
+		jmp DownScale4x4StrengthCodeCaveExit1
+
+		
+	}
+}
+
+void __declspec(naked) DownScale4x4StrengthCodeCave2()
+{
+	__asm {
+		// vertical road reflection
+		cmp dword ptr ds : [DownScale4x4ReturnAddress], 0x72E22E
+		jne DownScale4x4StrengthCodeCave2Part2
+		fdivr dword ptr ds : [ReflectionBlurStrength]
+		jmp DownScale4x4StrengthCodeCaveExit2
+
+	DownScale4x4StrengthCodeCave2Part2 :
+		// restores vertical depth of field
+		fdivr dword ptr ds : [DownScale4x4Strength]
+		jmp DownScale4x4StrengthCodeCaveExit2
+	}
+}
+
 void Init()
 {
 	// Read values from .ini
@@ -50,7 +115,7 @@ void Init()
 	// General
 	HDReflections = iniReader.ReadInteger("GENERAL", "HDReflections", 1);
 	PseudoXbox360Reflections = iniReader.ReadInteger("GENERAL", "PseudoXbox360Reflections", 1);
-	DisableReflectionBlur = iniReader.ReadInteger("GENERAL", "DisableReflectionBlur", 0);
+	ReflectionBlurStrength = iniReader.ReadFloat("GENERAL", "ReflectionBlurStrength", 0.0f);
 
 	if (HDReflections)
 	{
@@ -69,6 +134,20 @@ void Init()
 	if (PseudoXbox360Reflections)
 	{
 		injector::WriteMemory<uint8_t>(0x72ECE4, 0x02, true);
+	}
+
+	// ReflectionBlurStength
+	{
+		// Jumps
+		injector::MakeJMP(0x73C97C, ReflectionBlurStrengthCodeCave);
+		injector::MakeJMP(0x748880, DownScale4x4ReturnAddressCodeCave);
+		injector::MakeJMP(0x73CC29, DownScale4x4StrengthCodeCave1);
+		injector::MakeJMP(0x73CC3D, DownScale4x4StrengthCodeCave2);
+		// Allows blur to render at full resolution
+		injector::MakeNOP(0x713108, 2);
+		injector::MakeNOP(0x713141, 2);
+		injector::MakeNOP(0x73C359, 2);
+		injector::MakeNOP(0x73C371, 2);
 	}
 }
 	
